8ecdfb639ea27a0fa2a74ca9c881a9b8056b774d
Dogukan Elbasan <dogukanelbasan0@gmail.com>
2022-08-03 10:39:31 +0300

    first-commit

---
+++ coincalculator/src/main/java/com/dogukanelbasan/coincalculator/CoincalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.dogukanelbasan.coincalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoincalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoincalculatorApplication.class, args);
+	}
+
+}
---
+++ coincalculator/src/test/java/com/dogukanelbasan/coincalculator/CoincalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogukanelbasan.coincalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoincalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
